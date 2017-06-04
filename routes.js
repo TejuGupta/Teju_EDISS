6035ee3625f8ccd115a4b16e95f86a587003d6c9
Teju <Teju>
2017-06-04 14:45:06 -0400

    'project'

---
+++ routes.js
@@ -0,0 +1,206 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'mysql-useast1.c0kfhi1xeasa.us-east-1.rds.amazonaws.com',
+  user     : 'trachapu',
+  password : 'Mommie2129',
+  database : 'edisprod'
+});
+
+connection.connect();
+
+var validator = require('validator');
+
+module.exports = function(app, passport) {
+	
+	app.post(
+	'/login',  
+     function(req, res) { 
+			console.log('entered');
+			console.log(req.body.username+ " " + req.body.password);
+		  var params =[req.body.username,req.body.password];
+		  var sess= req.session;	  
+          var queries = connection.query("SELECT * from users where username =? and password=?", params,  
+		  function(err, rows, fields) {
+		  if (!err && rows.length > 0)
+		  {    
+			  var obj= '{“message”:“Welcome '+rows[0].firstname+'"}';
+			  req.session.username=req.body.username;
+			  req.session.touch();
+			  console.log("name"+req.session.username);
+			  console.log(obj+" ..");
+			  res.setHeader('Content-Type', 'application/json');
+			  res.send(obj);
+		  }			
+		  else if(err)
+		  {
+			  connection.end();
+		  }
+		  else		  
+		  {    
+			  var obj= '{“message”:“There seems to be an issue with the username/password combination that you entered"}';
+			  console.log(obj+" ..");
+			  req.session.touch();
+			  res.setHeader('Content-Type', 'application/json');
+			  res.send(obj);
+		  } 
+         });
+		
+	  } 
+	);
+	
+	
+	app.post(
+	'/add',  
+     function(req, res) { 
+			console.log('entered');
+			console.log(req.body.num1+ " " + req.body.num2);
+			var result = +req.body.num1 + +req.body.num2;
+			var name=req.session.username;
+			if(typeof name === 'undefined' || name == null)
+			{
+			  var obj= '{“message”:“You are not currently logged in”}'; 
+			  console.log(obj+" ..");
+			  req.session.touch();
+			  res.setHeader('Content-Type', 'application/json');
+			  res.send(obj);
+				
+			}
+	      
+		else  if(!validator.isNumeric(req.body.num1) || !validator.isNumeric(req.body.num2) || validator.isEmpty(req.body.num1) || validator.isEmpty(req.body.num2))
+		  {
+			  var obj= '{“message”:“The numbers you entered are not valid”}'; 
+			  console.log(obj+" ..");
+			  req.session.touch();
+			  res.setHeader('Content-Type', 'application/json');
+			  res.send(obj); 	 
+			  
+		  }
+		  
+		  else
+		  {
+			  var obj= '{“message”:“The action was successful”,"result":"'+result+'"}'; 
+			  console.log(obj+" ..");
+			  req.session.touch();
+			  res.setHeader('Content-Type', 'application/json');
+			  res.send(obj);
+			  
+		  }	  
+        } 
+	);
+	
+	
+	app.post(
+	'/multiply',  
+     function(req, res) { 
+			console.log('entered');
+			console.log(req.body.num1+ " " + req.body.num2);
+			var result = req.body.num1 * req.body.num2;
+			var name=req.session.username;
+			if(typeof name === 'undefined' || name == null)
+			{
+			  var obj= '{“message”:“You are not currently logged in”}'; 
+			  console.log(obj+" ..");
+			  req.session.touch();
+			  res.setHeader('Content-Type', 'application/json');
+			  res.send(obj);
+				
+			}
+	      
+		else  if(!validator.isNumeric(req.body.num1) || !validator.isNumeric(req.body.num2) || validator.isEmpty(req.body.num1) || validator.isEmpty(req.body.num2))
+		  {
+			  var obj= '{“message”:“The numbers you entered are not valid”}'; 
+			  console.log(obj+" ..");
+			  req.session.touch();
+			  res.setHeader('Content-Type', 'application/json');
+			  res.send(obj); 	 
+			  
+		  }
+		  
+		  else
+		  {
+			  var obj= '{“message”:“The action was successful”,"result":"'+result+'"}'; 
+			  console.log(obj+" ..");
+			  req.session.touch();
+			  res.setHeader('Content-Type', 'application/json');
+			  res.send(obj);
+			  
+		  }	 	  
+          
+	  } 
+	);
+	
+	
+	app.post(
+	'/divide',  
+     function(req, res) { 
+			console.log('entered');
+			console.log(req.body.num1+ " " + req.body.num2);
+			var result = req.body.num1 / req.body.num2;
+			var name=req.session.username;
+			var num= req.body.num2;
+		  	if(typeof name === 'undefined' || name == null)
+			{
+			  var obj= '{“message”:“You are not currently logged in”}'; 
+			  console.log(obj+" ..");
+			  req.session.touch();
+			  res.setHeader('Content-Type', 'application/json');
+			  res.send(obj);
+				
+			}
+	     
+		 else if(!validator.isNumeric(req.body.num1) || !validator.isNumeric(req.body.num2) || validator.isEmpty(req.body.num1) || validator.isEmpty(req.body.num2) || (num == 0))
+		  {
+			  var obj= '{“message”:“The numbers you entered are not valid”}'; 
+			  console.log(obj+" ..");
+			  req.session.touch();
+			  res.setHeader('Content-Type', 'application/json');
+			  res.send(obj); 	 
+			  
+		  }
+		  
+		  else 
+		  {
+			  var obj= '{“message”:“The action was successful”,"result":"'+result+'"}'; 
+			  console.log(obj+" ..");
+			  req.session.touch();
+			  res.setHeader('Content-Type', 'application/json');
+			  res.send(obj);
+			  
+		  }
+		 	  
+          
+	  } 
+	);
+	
+	app.post('/logout', function(req, res) {
+        console.log("logging out!");
+		
+			var name=req.session.username;
+		  	if(typeof name === 'undefined' || name == null)
+			{
+			  var obj= '{“message”:“You are not currently logged in”}'; 
+			  console.log(obj+" ..");
+			  res.setHeader('Content-Type', 'application/json');
+			  res.send(obj);
+				
+			}
+			
+			else
+			{
+			  var obj= '{“message”:“You have been successfully logged out"}'; 
+			  console.log(obj+" ..");		
+			  res.setHeader('Content-Type', 'application/json');
+			  res.send(obj);
+			   req.logout();
+               req.session.destroy();
+				
+			}	
+       
+        
+    });
+
+
+};
+
+
+
---
+++ server.js
@@ -0,0 +1,33 @@
+// server set ups
+// all that we need
+var express  = require('express');
+var app      = express();
+var port     = process.env.PORT || 8080;
+var mysql = require('mysql');
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
+
+
+
+// set up express
+app.use(morgan('dev')); 
+app.use(cookieParser()); 
+app.use(bodyParser()); 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(session({ 
+name: 'session-cookie',
+secret: 'nothing',
+saveUninitialized: true,
+cookie: { maxAge: 900000 },
+rolling: true,
+resave:true })); // set session secret and maxAge with rolling set as true
+
+// routes 
+require('./routes.js')(app) // routes contained within the application
+
+app.listen(port);
+console.log('My first node app listening on port' + port);
